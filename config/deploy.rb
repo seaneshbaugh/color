4762abb17c4e74555cfa141144f975ca29a7d2b1
Sean Eshbaugh <seaneshbaugh@gmail.com>
2013-10-14 12:11:28 -0500

    Updated deploy scripts.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,20 +1,7 @@
 set :application, 'color'
 set :repo_url, 'git@github.com:seaneshbaugh/color.git'
 
-# ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }
-
-set :deploy_to, '/home/seaneshb/color_public_html'
 set :scm, :git
-
-# set :format, :pretty
-# set :log_level, :debug
-# set :pty, true
-
-# set :linked_files, %w{config/database.yml}
-# set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
-
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-# set :keep_releases, 5
 
 namespace :deploy do
   desc 'Restart application'
