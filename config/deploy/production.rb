7714880dc941c739da340068131156961526b283
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-10-24 17:22:25 -0500

    Removed capistrano stuff.

--- config/deploy/production.rb
+++
@@ -1,9 +0,0 @@
-set :stage, :production
-
-server 'seaneshbaugh.com', user: 'seaneshb', roles: %w{web app}
-
-set :branch, 'master'
-
-set :deploy_to, '/home/seaneshb/color'
-
-set :tmp_dir, "/home/seaneshb/tmp"
