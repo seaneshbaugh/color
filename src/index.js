08fd9b95ac4890afc3da73f8126cf519e36b354b
Sean Eshbaugh <seaneshbaugh@gmail.com>
2019-10-24 22:31:19 -0500

    Added hot module reloading.

--- src/index.js
+++ src/index.js
@@ -2,3 +2,7 @@ import "./js/app.js";
 import "./css/app.scss";
 import "./text/humans.txt";
 import "./text/robots.txt";
+
+if (typeof(module.hot) !== "undefined") {
+  module.hot.accept();
+}
--- src/server/server.js
+++ src/server/server.js
@@ -2,6 +2,7 @@ import path from "path";
 import express from "express";
 import webpack from "webpack";
 import webpackDevMiddleware from "webpack-dev-middleware";
+import webpackHotMiddleware from "webpack-hot-middleware";
 import config from "../../webpack.dev.config.js";
 
 const DIST_DIR = __dirname;
@@ -13,6 +14,8 @@ const compiler = webpack(config);
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath
 }));
+
+app.use(webpackHotMiddleware(compiler));
 
 app.get("*", (req, res, next) => {
   compiler.outputFileSystem.readFile(HTML_FILE, (err, result) => {
